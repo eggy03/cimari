ebd92a63f6d1a40565e073c55fffbbafe967eb4b
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-04-11 11:18:59 +0530

    docs: mention that Cimv2 is for WMI Win32 classes
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/cimari/shell/query/Cimv2.java
+++ src/main/java/io/github/eggy03/cimari/shell/query/Cimv2.java
@@ -33,7 +33,7 @@ import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Enum representing the predefined WMI (CIM) queries for the classes available in the {@code root/cimv2} namespace.
+ * Enum representing the predefined WMI Win32 Class queries for the classes available in the {@code root/cimv2} namespace.
  * <p>
  * Each constant holds a PowerShell query that queries a specific class in the namespace
  * and returns the result in JSON format.
